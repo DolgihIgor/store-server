08d9c6ad35355cd759c2a334c7b9622d11a27ed6
DolgihIgor <igor_dolgih@inbox.ru>
2023-06-09 14:53:19 +0700

    Добавили модели для товаров и категорий

--- products/models.py
+++ products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    """
+    Модель для категорий, поле name должно быть уникальным.
+    Поле описание необязательно для заполнения.
+    Поле id создается Django автоматически с автоинкрементом.
+    """
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Product(models.Model):
+    """ Модель для товаров """
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+    # Поле с ценой, 6 знаков до запятой, 2 знака после
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    # Количество, положительное, по умолчанию 0
+    quantity = models.PositiveIntegerField(default=0)
+
+    # Изображение, загружается в папку products_images
+    image = models.ImageField(upload_to='products_images')
+
+    # Категории - это ссылка на категорию, при удалении категории,
+    # CASCADE - каскадное удаление всех товаров с этой категорией
+    # если указать PROTECT - не даст удалить, пока существует ссылка на категорию
+    # SET_DEFAULT, default = Null - заполнит по умолчанию нулями
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
